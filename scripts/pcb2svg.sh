2e0a12f86f4d6230ca8fa7e6d1d8fa3e64091f88
Tom <thomas@w4xzr.xyz>
2020-07-03 17:28:29 -0400

    modified:   scripts/pcb2svg.sh

--- scripts/pcb2svg.sh
+++ scripts/pcb2svg.sh
@@ -6,10 +6,12 @@ mkdir -p ../Gerbers  > /dev/null 2>&1
 
 python3 plot_gerbers.py "../$TEMP.kicad_pcb"
 echo python3 plot_board.py "../$TEMP.kicad_pcb"
+
 mv "../plot/$TEMP-Front.png" "../Images/Front.png"
 mv "../plot/$TEMP-Back.png" "../Images/Back.png"
-mv ../plot/$TEMP\_gerbers.zip ../Gerbers/  > /dev/null 2>&1
-ls .. && ls ../Gerbers
+mv ../$TEMP\_gerbers.zip ../Gerbers/ > /dev/null 2>&1
+
 rm ../plot/*
 rm -d ../plot
+
 ./travis_push.sh
